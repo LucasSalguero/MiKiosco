187b4c04e452bc00fca3e26db62bc1d3d94747af
LucasSalguero <lucassalguero01@gmail.com>
2026-09-18 18:53:08 -0300

    Tablas y schemas

---
+++ MiKiosco/prisma/migrations/20260918215039_init/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE "Producto" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "precio" DECIMAL(65,30) NOT NULL,
+    "activo" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Producto_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Venta" (
+    "id" SERIAL NOT NULL,
+    "fecha" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "total" DECIMAL(65,30) NOT NULL,
+    "sincronizada" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Venta_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VentaItem" (
+    "id" SERIAL NOT NULL,
+    "ventaId" INTEGER NOT NULL,
+    "productoId" INTEGER NOT NULL,
+    "cantidad" INTEGER NOT NULL,
+    "precioUnitario" DECIMAL(65,30) NOT NULL,
+
+    CONSTRAINT "VentaItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "VentaItem" ADD CONSTRAINT "VentaItem_ventaId_fkey" FOREIGN KEY ("ventaId") REFERENCES "Venta"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VentaItem" ADD CONSTRAINT "VentaItem_productoId_fkey" FOREIGN KEY ("productoId") REFERENCES "Producto"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
